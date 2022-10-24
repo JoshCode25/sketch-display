96eebc4774f43270dcad6c7e986a1cc376cef8c5
JoshG <joshcode25@gmail.com>
2022-10-24 06:25:30 -0500

    Updates

---
+++ precache-manifest.b46a43fed07d17984a430f0457e7889e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bbf62b49905099fbef95f8ca45156ec",
+    "url": "/sketch-display/index.html"
+  },
+  {
+    "revision": "3445d7acb419b7e366a1",
+    "url": "/sketch-display/static/css/2.85c36af2.chunk.css"
+  },
+  {
+    "revision": "28ae0ace039fa0b5bfa2",
+    "url": "/sketch-display/static/css/main.e0dbbc79.chunk.css"
+  },
+  {
+    "revision": "3445d7acb419b7e366a1",
+    "url": "/sketch-display/static/js/2.dc2ea2e7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/sketch-display/static/js/2.dc2ea2e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28ae0ace039fa0b5bfa2",
+    "url": "/sketch-display/static/js/main.0654dd37.chunk.js"
+  },
+  {
+    "revision": "1c22c5848a1bd09b84a6",
+    "url": "/sketch-display/static/js/runtime-main.bffcf981.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/sketch-display/static/media/SEGA.12a3db8b.woff"
+  }
+]);
