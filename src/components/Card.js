9878fed7a7479124e47817fc646e7aa572d07469
JoshG <joshcode25@gmail.com>
2022-10-24 06:20:04 -0500

    added white background behind images

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,11 @@ import React from 'react';
 const Card = ({ name, project }) => {
   return (
     <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
-      <img style={{width: "300px"}} alt={`img - ${name}`} src={`https://res.cloudinary.com/joshcode25/image/upload/v1/${project}/${name}.png`} /> 
+      <img 
+        className='project-image'
+        alt={`img - ${name}`} 
+        src={`https://res.cloudinary.com/joshcode25/image/upload/v1/${project}/${name}.png`}
+      /> 
       <div>
         <h2>{name}</h2>
         <p>{project}</p>
